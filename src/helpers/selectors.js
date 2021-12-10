795aeab140270373ed4c4405f0098cee4a95100e
keepitclassick <keepitclassick@gmail.com>
2021-12-10 13:52:31 -0500

    install react hooks testing and create visual mode hook file/test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,3 +7,22 @@ export function getAppointmentsForDay(state, day) {
   }
   return appointments;
 }
+
+export function getInterview(state, interview) {
+  if(!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer;
+
+  for (const id in state.interviewers) {
+    if (Number(id) === interviewerId) {
+      return (
+        {
+          student: interview.student,
+          interviewer: state.interviewers[id]
+        }
+      )
+    }
+  }
+}
