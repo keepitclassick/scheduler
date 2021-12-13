6a54c07e53cdc9f862f1794f72ba4eb990531c8e
keepitclassick <keepitclassick@gmail.com>
2021-12-13 14:55:43 -0500

    edit implemented

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show"
 import Empty from "./Empty"
 import Form from "./Form"
 import Status from "./Status"
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -14,6 +15,9 @@ export default function Appointment(props) {
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING"
+  const DELETING = "DELETE";
+  const CONFIRM = "CONFIRM";
+  const EDITING = "EDITING";
 
   const { mode, transition, back } = useVisualMode( props.interview? SHOW : EMPTY );
 
@@ -26,13 +30,30 @@ export default function Appointment(props) {
     props.bookInterview(props.id, interview)
       .then(() => transition(SHOW));
   }
+
+
+  function deleteInterview() {
+    transition(DELETING);
+    props.cancelInterview(props.id)
+      .then(() => transition(EMPTY));
+  } 
+
+
+ 
+
+
   return (
     <article className="appointment">
       <Header time={ props.time } />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && <Show student={ props.interview.student } interviewer={ props.interview.interviewer }/>}    
+      {mode === SHOW && <Show student={ props.interview.student } interviewer={ props.interview.interviewer } onDelete={()=>transition(CONFIRM)} onEdit={()=>transition(EDITING)}/>}    
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={ back } onSave={ save }/>}
+      {mode === CONFIRM && <Confirm message= 'Are you sure you would like to delete?' onCancel={back} onConfirm={deleteInterview} />}
+      {mode === EDITING && <Form name={props.interview.student} interviewer={props.interview.interviewer.id} interviewers={props.interviewers} onCancel={() => transition(SHOW)} onSave={save} />}
       {mode === SAVING && <Status message= 'Saving'/>}
+      {mode === DELETING && <Status message= 'Deleting' />}
+      
+  
     </article>
   )
 };
