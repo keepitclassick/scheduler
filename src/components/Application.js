d61754cd137ada8d442ee73137850f3661ba79ca
keepitclassick <keepitclassick@gmail.com>
2021-12-13 16:05:11 -0500

    create useApplicationData hook and app now updates remaining appointments on each day

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,60 +7,13 @@ import Appointment from "components/Appointment";
 import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors"
 import useVisualMode from "hooks/useVisualMode"
+import useApplicationData from "hooks/useApplicationData";
 
 
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments:{},
-    interviewers:{}
-  })
 
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then(all => {
-      setState(state => ({...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    })
-  }, []);
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-   
-    return axios.put(`/api/appointments/${id}`, {interview})
-    .then(response => setState(state => ({ ...state, appointments })));
-  }
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-
-    return axios.delete(`/api/appointments/${id}`)
-      .then(response => setState(state => ({ ...state, appointments })));
-  }
-   
- 
-  
-
-const setDay = day => setState({ ...state, day });
+const { state, setDay, bookInterview, cancelInterview } = useApplicationData()
 
 const appointmentsForDay = getAppointmentsForDay(state, state.day);
 
